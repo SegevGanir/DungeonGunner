e9bf04774d90ef7136fa850bda3b274e90e895b5
SegevGanir <Segev@ganir.net>
2023-11-23 12:14:19 -0800

    New Window

--- DungeonGunner/Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
+++ DungeonGunner/Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class RoomNodeGraphEditor : MonoBehaviour
+public class RoomNodeGraphEditor : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[MenuItem("Room Node Graph Editor", menuItem = "Window / Dungeon Editor / Room Node Graph Editor")]
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private static void OpenWindow()
+	{
+		GetWindow<RoomNodeGraphEditor>("Room Node Graph Editor");
+	}
 }
