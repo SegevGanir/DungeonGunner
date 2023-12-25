c7ee5bc908cb92f39f153d58dfc434f529a6915e
SegevGanir <Segev@ganir.net>
2023-12-25 14:14:42 -0800

    Update RoomNodeGraphEditor.cs

--- DungeonGunner/Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
+++ DungeonGunner/Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -1,11 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Callbacks;
 using UnityEditor;
 
 public class RoomNodeGraphEditor : EditorWindow
 {
 	private GUIStyle roomNodeStyle;
+	private static RoomNodeGraphSO currentRoomNodeGraph;
+	private RoomNodeTypeListSO roomNodeTypeList;
 
 	// Node Layout values
 	private const float nodeWidth = 160f;
@@ -28,19 +29,70 @@ public class RoomNodeGraphEditor : EditorWindow
 		roomNodeStyle.normal.textColor = Color.white;
 		roomNodeStyle.padding = new RectOffset(nodePadding, nodePadding, nodePadding, nodePadding);
 		roomNodeStyle.border = new RectOffset(nodeBorder, nodeBorder, nodeBorder, nodeBorder);
+
+		// Load Room node types
+		roomNodeTypeList = GameResources.Instance.roomNodeTypeList;
 	}
+
+	/// <summary>
+	/// Open the room node graph editor window if a room node graph scriptable object asset is double clicked in the inspector
+	/// </summary>
+	[OnOpenAsset(0)] // Need the namespace UnityEditor.Callbacks
+	private static bool OnDoubleClickAsset(int instanceID, int line)
+	{
+		RoomNodeGraphSO roomNodeGraph = EditorUtility.InstanceIDToObject(instanceID) as RoomNodeGraphSO;
+
+		if (roomNodeGraph != null)
+		{
+			OpenWindow();
+
+			currentRoomNodeGraph = roomNodeGraph;
+
+			return true;
+		}
+		return false;
+	}
+
 
 	/// <summary>
 	/// Draw Editor GUI
 	/// </summary>
 	private void OnGUI()
 	{
-		GUILayout.BeginArea(new Rect(new Vector2(100f, 100f), new Vector2(nodeWidth, nodeHeight)), roomNodeStyle);
-		EditorGUILayout.LabelField("Node 1");
-		GUILayout.EndArea();
 		
-		GUILayout.BeginArea(new Rect(new Vector2(300f, 300f), new Vector2(nodeWidth, nodeHeight)), roomNodeStyle);
-		EditorGUILayout.LabelField("Node 2");
-		GUILayout.EndArea();
+		// If a scriptable object of type RoomNodeGraphSO has been selected then process
+		if (currentRoomNodeGraph != null)
+		{
+			// Process Events
+			ProcessEvents(Event.current);
+
+			//Draw room nodes
+			DrawRoomNodes();
+		}
+
+		if (GUI.changed)
+			Repaint();
+	}
+
+	private void ProcessEvents(Event currentEvent)
+	{
+		ProcessRoomNodeGraphEvents(currentEvents);
+	}
+	
+	/// <summary>
+	/// Process Room Node Graph Events
+	/// </summary>
+	private void ProcessRoomNodeGraphEvents(Event currentEvent)
+	{
+		switch (currentEvent.type)
+		{
+			// Process Mouse Down Events
+			case EventType.MouseDown:
+				ProcessMouseDownEvent(currentEvent);
+				break;
+
+			default:
+				break;
+		}
 	}
 }
